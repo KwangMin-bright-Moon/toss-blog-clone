25c4e2d0a75109a0d18af3d6c1cd0e43274c3f9f
Jason <jasondal91@gmail.com>
2023-04-20 18:09:13 +0900

    기존 post로 된 네이밍 article로 수정

--- src/mocks/resolvers/mock.design.js
+++ src/mocks/resolvers/mock.design.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const developPosts = [
+export const designArticles = [
   {
     id: uuidv4(),
     createdAt: new Date(),
--- src/mocks/resolvers/mock.tech.js
+++ src/mocks/resolvers/mock.tech.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export const designPosts = [
+export const techArticles = [
   {
     id: uuidv4(),
     createdAt: new Date(),
