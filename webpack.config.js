f9a3d58f0d4205b93c454cbb6108cd19823c2f80
Jason <jasondal91@gmail.com>
2023-04-20 20:34:15 +0900

    webpack 추가 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     main: './src/index.js',
-    worker: './mockServiceWorker.js',
+    mockServiceWorker: './mockServiceWorker.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
-
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    port: 'auto',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'dist/index.html',
+    }),
+  ],
   devtool: 'source-map',
 };
