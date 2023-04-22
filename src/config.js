f0013f7af34b284444004acaedca8e1567934ca0
Jason <jasondal91@gmail.com>
2023-04-22 15:59:08 +0900

    GET url 상수 처리 및 config.js에서 관리

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const TECH_ARTICLES = 'dist/tech/articles';
+export const TECH_ARTICLE_DETAIL = 'dist/tech/article/:articleId';
+export const DESIGN_ARTICLES = 'dist/design/articles';
+export const DESIGN_ARTICLE_DETAIL = 'dist/design/article/:articleId';
