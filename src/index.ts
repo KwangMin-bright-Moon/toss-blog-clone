54ebb67d417bab6472d1332c7ed91d85687c802a
Jason <jasondal91@gmail.com>
2023-05-23 18:41:41 +0900

    refactor: router 구조 변경에 따른 수정

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
-import {
-  DESIGN_ARTICLES,
-  DESIGN_ARTICLE_DETAIL,
-  TECH_ARTICLES,
-  TECH_ARTICLE_DETAIL,
-} from './config';
 import { worker } from './mocks/browser';
 import Router from './router/router';
 import ArticleView from './view/article.view';
 import ArticleListView from './view/articleList.view';
-
-const $main = document.getElementById('root');
-
-const components = {
-  home: () => new ArticleListView(TECH_ARTICLES, 'tech').render($main),
-  techArticles: () => new ArticleListView(TECH_ARTICLES, 'tech').render($main),
-  techArticleDetail: (params: any) =>
-    new ArticleView(TECH_ARTICLE_DETAIL, params).render($main),
-  designArticles: () =>
-    new ArticleListView(DESIGN_ARTICLES, 'design').render($main),
-  designArticleDetail: (params: any) =>
-    new ArticleView(DESIGN_ARTICLE_DETAIL, params).render($main),
-  notFound: () => ($main.innerText = 'NotFound'),
-};
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
@@ -30,10 +10,15 @@ if (process.env.NODE_ENV === 'development') {
 const router = new Router();
 
 router
-  .addRouter('/', components.home)
-  .addRouter('/tech', components.techArticles)
-  .addRouter('/tech/article/:articleId', components.techArticleDetail)
-  .addRouter('/design', components.designArticles)
-  .addRouter('/design/article/:articleId', components.designArticleDetail)
-  .setNotFound(components.notFound)
+  .addRouter({ path: '/', view: new ArticleListView('tech', 'root') })
+  .addRouter({ path: '/tech', view: new ArticleListView('tech', 'root') })
+  .addRouter({
+    path: '/tech/article/:articleId',
+    view: new ArticleView('tech', 'root'),
+  })
+  .addRouter({ path: '/design', view: new ArticleListView('design', 'root') })
+  .addRouter({
+    path: '/design/article/:articleId',
+    view: new ArticleView('design', 'root'),
+  })
   .start();
